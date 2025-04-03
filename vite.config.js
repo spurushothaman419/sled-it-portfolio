5203ea07938dd660021db8e038e6da71e2f61a04
spurushothaman419 <spurushothaman419@gmail.com>
2025-04-02 18:44:10 -0700

    Fix GitHub Pages deploy permission denied error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sled-it-portfolio/', // ✅ Use your repo name prefixed with /
+  base: '/sled-it-portfolio/',
   plugins: [react()],
 })

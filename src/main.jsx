2955720d66aaed99a6a6657aba123cc4f028e773
spurushothaman419 <spurushothaman419@gmail.com>
2025-04-02 17:28:54 -0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import CybersecurityPortfolio from "./CybersecurityPortfolio";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CybersecurityPortfolio />
+  </React.StrictMode>
+);
